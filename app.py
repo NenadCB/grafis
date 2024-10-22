364fbf1a0042158c9d22d8bb0fb9b0ac4107ac02
NenadCB <161453776+NenadCB@users.noreply.github.com>
2024-10-22 11:01:23 +0000

    dodan primer izpisa rezultatov v zanki

--- app.py
+++ app.py
@@ -2,10 +2,12 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+geslo = 'sila'
+opis = 'sila je moja najljubša igra'
 
 @app.route('/')
 def index():
-    return render_template('home.html')
+    return render_template('home.html', geslo=geslo, opis=opis)
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', port=5000, debug=True)
