cdb4cea4d03814848a52db6767c92d48e314e954
NenadCB <161453776+NenadCB@users.noreply.github.com>
2024-10-23 11:53:56 +0200

    povezava z bazo OK

--- database.py
+++ database.py
@@ -8,3 +8,13 @@ from flask import (
   make_response
 )
 
+from sqlalchemy import create_engine, text
+
+# Database
+engine = create_engine("postgresql://grafis_owner:RTogb40WyXmG@ep-autumn-sunset-a8e3slx5.eastus2.azure.neon.tech/grafis?options=endpoint%3Dep-autumn-sunset-a8e3slx5"
+)
+
+
+with engine.connect() as conn:
+    result = conn.execute(text('select * from slovar'))
+    print(result.all())
