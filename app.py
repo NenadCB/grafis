6326716ded32bb9272f214884b16f728a1b9ba7e
NenadCB <161453776+NenadCB@users.noreply.github.com>
2024-10-23 06:06:01 +0000

    prenos na svoj x1

--- app.py
+++ app.py
@@ -5,6 +5,14 @@ app = Flask(__name__)
 geslo = 'sila'
 opis = 'sila je moja najljubša igra'
 
+from flask_sqlalchemy import SQLAlchemy
+
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://grafis_owner:RTogb40WyXmG@ep-autumn-sunset-a8e3slx5.eastus2.azure.neon.tech/grafis?sslmode=require"
+
+# Database
+db = SQLAlchemy(app)
+
+
 @app.route('/')
 def index():
     return render_template('home.html', geslo=geslo, opis=opis)
---
+++ database.py
@@ -0,0 +1,10 @@
+# Imports
+from flask import (
+  Flask,
+  abort,
+  redirect,
+  url_for,
+  render_template,
+  make_response
+)
+
