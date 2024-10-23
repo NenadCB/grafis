38e05c87a3ce4164fcafe8fe61a35fc170336356
NenadCB <161453776+NenadCB@users.noreply.github.com>
2024-10-23 15:12:42 +0200

    podatki iz baze na strani

--- app.py
+++ app.py
@@ -5,10 +5,20 @@ app = Flask(__name__)
 geslo = 'sila'
 opis = 'sila je moja najljubša igra'
 
+from sqlalchemy import create_engine, text
+
+# Database
+engine = create_engine("postgresql://grafis_owner:RTogb40WyXmG@ep-autumn-sunset-a8e3slx5.eastus2.azure.neon.tech/grafis?options=endpoint%3Dep-autumn-sunset-a8e3slx5"
+)
+
+
+with engine.connect() as conn:
+    result = conn.execute(text('select * from slovar'))
+    podatki = list(result.all())
 
 @app.route('/')
 def index():
-    return render_template('home.html', geslo=geslo, opis=opis)
+    return render_template('home.html', geslo=geslo, opis=opis, podatki=podatki)
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', port=5000, debug=True)
