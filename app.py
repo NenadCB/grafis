84f18cc46e6141435c66207e99f40b0cb62aa2bd
NenadCB <161453776+NenadCB@users.noreply.github.com>
2024-11-20 09:46:57 +0100

    opis gesla

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request
-from database import load_gesla
+from database import load_gesla, load_geslo
 
 app = Flask(__name__)
 
@@ -14,6 +14,12 @@ def index():
             podatki = load_gesla(iskalnik)
 
     return render_template('home.html', podatki=podatki)
+
+@app.route('/geslo/<id>')
+def show_geslo(id):
+    podatki = {}
+    podatki = load_geslo(id)
+    return render_template('geslo.html', podatki=podatki)
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', port=5000, debug=True)
--- database.py
+++ database.py
@@ -21,4 +21,75 @@ def load_gesla(iskalnik):
            rows.append(row._mapping)
     return rows
 
+def load_geslo(id):
+    select = 'select * from slovarfizika where id = :val'
+    with engine.connect() as conn:
+        result = conn.execute(text(select), parameters=dict(val=id))
+        rows = result.all()
+        if len(rows) == 0:
+            return None
+        else:
+            return rows[0]._mapping
+        
+def import_data():
+    with open('podatki.txt', encoding='UTF-8') as vhodna:
+        for vrstica in vhodna:
+            vrstica = vrstica.strip('\n')
+            vrstica = vrstica.strip('\\')
+            podatki = vrstica.split('&')
+            geslo_ang = podatki[0].strip()
+            geslo_slo = podatki[1].strip()
+            definicija_ang = podatki[2].strip()
+            definicija_slo = podatki[3].strip()
+            iso = podatki[4].strip('')
+            formula = ''
+            oznaka = ''
+            enota = ''
+            opombe = ''            
+            select = f"""insert into slovarfizika (
+                            geslo_ang, 
+                            geslo_slo, 
+                            definicija_ang, 
+                            definicija_slo, 
+                            iso, 
+                            formula, 
+                            oznaka, 
+                            enota, 
+                            opombe)
+                        values (
+                            '{geslo_ang}',
+                            '{geslo_slo}',
+                            '{definicija_ang}',
+                            '{definicija_slo}',
+                            '{iso}',
+                            '{formula}',
+                            '{oznaka}',
+                            '{enota}',
+                            '{opombe}'
+                            )
+                    """
+            with engine.connect() as conn:
+                result = conn.execute(text(select))
+                conn.commit()
+
+def create_slovar_fizika():
+    select =  '''create table slovarfizika (
+                               id serial primary key,
+                               geslo_ang varchar(100), 
+                               geslo_slo varchar(100), 
+                               definicija_ang varchar(1000), 
+                               definicija_slo varchar(1000), 
+                               iso varchar(100), 
+                               formula varchar(300), 
+                               oznaka varchar(10), 
+                               enota varchar(10), 
+                               opombe varchar(1000))'''
+    with engine.connect() as conn:
+          result = conn.execute(text(select))
+
+#print('test')
+
+#create_slovar_fizika()
+#import_data()
 #print(load_gesla())
+#print(load_geslo(48))
